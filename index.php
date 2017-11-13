22515aa962349a8b9ace921adc45d8e264f7f00c
JordiJacas <jjacasventura@iesesteveterradas.cat>
2017-11-13 17:15:15 +0100

    dasdaX

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+<form action="consulta.php" method="post">
+Pais: <select name="code">
+	
+<?php
+ 		# (1.1) Connectem a MySQL (host,usuari,contrassenya)
+ 		$conn = mysqli_connect('localhost','root','Contrasenya9');
+ 
+ 		# (1.2) Triem la base de dades amb la que treballarem
+ 		mysqli_select_db($conn, 'world');
+ 
+ 		# (2.1) creem el string de la consulta (query)
+ 		$consulta = "SELECT Name, Code FROM country;";
+ 
+ 		# (2.2) enviem la query al SGBD per obtenir el resultat
+ 		$resultat = mysqli_query($conn, $consulta);
+ 
+ 		# (2.3) si no hi ha resultat (0 files o bé hi ha algun error a la sintaxi)
+ 		#     posem un missatge d'error i acabem (die) l'execució de la pàgina web
+ 		if (!$resultat) {
+     			$message  = 'Consulta invàlida: ' . mysqli_error() . "\n";
+     			$message .= 'Consulta realitzada: ' . $consulta;
+     			die($message);
+ 		}
+ 		# (3.2) Bucle while
+ 		while( $registre = mysqli_fetch_assoc($resultat) )
+ 		{
+ 			echo"<option value='".$registre["Code"]."'>".$registre["Name"] ."</option>";
+ 		}
+ 	?>
+	  </select>
+	  <input type="submit" name="submit"/>
+</form>
+</body>
+</html>
+
+
